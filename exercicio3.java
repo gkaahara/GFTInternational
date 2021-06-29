0c9200255544c8c9858c93717e164637a693ba6a
Gabriel Kaahara <68751904+gkaahara@users.noreply.github.com>
2021-06-29 16:02:08 -0300

    Add files via upload

---
+++ exercicio3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class exercicio3 {
+    int x=0;
+    public static void main(String[] args) {
+        exercicio3 imposto = new exercicio3();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite um valor: ");
+        
+    }
+    public class ICMS{
+
+    }
+    public class IPI{
+
+    }
+    public class COFINS{
+
+    }
+    static void calculaImposto(double valor){
+
+    }
+
+}
